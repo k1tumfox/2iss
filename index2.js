fe454ec1a568a802e83cf045bab40a75f3db20de
Tenyson Kwon <tenysonkwon@gmail.com>
2023-01-25 12:36:14 -0500

    fetchMyIP promised

--- index2.js
+++ index2.js
@@ -1,6 +1,6 @@
 
-// const { fetchMyIP, fetchCoordsByIP, fetchIssTimes, nextISSTimesForMyLocation } = require('./iss');
-const { nextISSTimesForMyLocation } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchIssTimes, nextISSTimesForMyLocation } = require('./iss_promised');
+// const { nextISSTimesForMyLocation } = require('./iss');
 
 // fetchMyIP((err, ip) => {
 //   // err ? console.log("It didn't work!", err) : ip;
@@ -15,22 +15,27 @@ const { nextISSTimesForMyLocation } = require('./iss');
 // const { latitude, longitude } = { latitude: 43.467517, longitude: -79.6876659 };
 
 
-const printPassTimes = function(passTimes) {
-  for (const pass of passTimes) {
-    const datetime = new Date(0);
-    datetime.setUTCSeconds(pass.risetime);
-    const duration = pass.duration;
-    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
-  }
-}
+fetchMyIP()
+  .then(body => console.log(body.data.ip));
 
-nextISSTimesForMyLocation((error, passTimes) => {
-  if (error) {
-    return console.log("It didn't work!", error);
-  }
-  // success, print out the deets!
-  printPassTimes(passTimes);
-});
+
+
+// const printPassTimes = function(passTimes) {
+//   for (const pass of passTimes) {
+//     const datetime = new Date(0);
+//     datetime.setUTCSeconds(pass.risetime);
+//     const duration = pass.duration;
+//     console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+//   }
+// }
+
+// nextISSTimesForMyLocation((error, passTimes) => {
+//   if (error) {
+//     return console.log("It didn't work!", error);
+//   }
+//   // success, print out the deets!
+//   printPassTimes(passTimes);
+// });
 
 
 
--- iss_promised.js
+++ iss_promised.js
@@ -9,80 +9,83 @@ const axios = require('axios');
  */
 
 
-const fetchMyIP = (callback) => { 
-  axios.get('https://api.ipify.org?format=json')
-    .then(res => {
-      if (res.status !== 200) {
-        return callback(Error(`Status Code ${res.status} when fetching IP: ${res.data}`), null);
-      } else {
-        return callback(null, res.data.ip);
-      }
-    })
-    .catch(err => {
-      return callback(err, null);
-    });
-};
+// const fetchMyIP = (callback) => { 
+//   axios.get('https://api.ipify.org?format=json')
+//     .then(res => {
+//       if (res.status !== 200) {
+//         return callback(Error(`Status Code ${res.status} when fetching IP: ${res.data}`), null);
+//       } else {
+//         return callback(null, res.data.ip);
+//       }
+//     })
+//     .catch(err => {
+//       return callback(err, null);
+//     });
+// };
 
+const fetchMyIP = function() {
+  return axios.get('https://api.ipify.org?format=json');
+};
 
 // fetchMyIP((err, ip) => {
 //   err ? console.log("It didn't work!", err) : console.log('It worked! Returned IP: ', ip);
 // });
 
 
-const fetchCoordsByIP = (ip, callback) => {
-  axios.get(`http://ipwho.is/${ip}`)
-    .then(res => {
-      if (!res.data.success) {
-        return callback(Error(`${res.data.message}: ${res.data.ip}`), null);
-      } else {
-        const { latitude, longitude } = res.data;
-        // const coords = {
-        //   latitude: res.data.latitude,
-        //   longitude: res.data.longitude
-        // };
-        return callback(null, { latitude, longitude }); 
-      }
-    })
-    .catch(err => {
-      return callback(err, null);
-    });
-};
+// const fetchCoordsByIP = (ip, callback) => {
+//   axios.get(`http://ipwho.is/${ip}`)
+//     .then(res => {
+//       if (!res.data.success) {
+//         return callback(Error(`${res.data.message}: ${res.data.ip}`), null);
+//       } else {
+//         const { latitude, longitude } = res.data;
+//         // const coords = {
+//         //   latitude: res.data.latitude,
+//         //   longitude: res.data.longitude
+//         // };
+//         return callback(null, { latitude, longitude }); 
+//       }
+//     })
+//     .catch(err => {
+//       return callback(err, null);
+//     });
+// };
 
 
-const fetchIssTimes = (coords, callback) => {
-  const { latitude, longitude } = coords;
-  axios.get(`https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`)
-    .then(res => {
-      if (res.data.message !== 'success') {
-        return callback(Error(`Error occurred: ${res.data.message}`), null);
-      } else {
-        return callback(null, res.data.response);
-      }
-    })
-    .catch(err => {
-      return callback(err, null);
-    });
-};
-
-
-
-const nextISSTimesForMyLocation = function(callback) {
-  fetchMyIP((err, ip) => {
-    if (err) {
-      return callback(err, null);
-    } 
-    fetchCoordsByIP(ip, (err, location) => {
-      err && callback(err, null);
-      fetchIssTimes(location, (err, nextPasses) => {
-        err && callback(err, null);
-        callback(null, nextPasses);
-      });
-    });
-  });
-};
+// const fetchIssTimes = (coords, callback) => {
+//   const { latitude, longitude } = coords;
+//   axios.get(`https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`)
+//     .then(res => {
+//       if (res.data.message !== 'success') {
+//         return callback(Error(`Error occurred: ${res.data.message}`), null);
+//       } else {
+//         return callback(null, res.data.response);
+//       }
+//     })
+//     .catch(err => {
+//       return callback(err, null);
+//     });
+// };
 
 
 
+// const nextISSTimesForMyLocation = function(callback) {
+//   fetchMyIP((err, ip) => {
+//     if (err) {
+//       return callback(err, null);
+//     } 
+//     fetchCoordsByIP(ip, (err, location) => {
+//       err && callback(err, null);
+//       fetchIssTimes(location, (err, nextPasses) => {
+//         err && callback(err, null);
+//         callback(null, nextPasses);
+//       });
+//     });
+//   });
+// };
 
 
-module.exports = { nextISSTimesForMyLocation };
+
+
+module.exports = { fetchMyIP };
+// module.exports = { fetchMyIP, fetchCoordsByIP, fetchIssTimes, nextISSTimesForMyLocation };
